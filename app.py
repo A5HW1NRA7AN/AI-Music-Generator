8e2e05537924d1873423da3f201c08d986cc9313
A5HW1NRA7AN <rajanashwin6@gmail.com>
2025-04-02 07:53:47 +0530

    The project source code has been pushed

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'mid', 'midi'}
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    midi_file = None
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            midi_file = url_for('static', filename=f'uploads/{filename}')
+            print("Uploaded MIDI File URL:", midi_file)
+    return render_template('index.html', midi_file=midi_file)
+
+if __name__ == '__main__':
+    app.run(debug=True)
